e2f9ed152499b8f5be0cbccfa8952e88df144e71
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-04-15 22:52:04 +0200

    feat(pyaoc): add basic structure for python exercise and utils
    This commit introduces a basic structure for creating a python exercise and utils. The `create_python_exercise` function now returns a template for a python exercise script, which includes argument parsing and data loading. The `create_python_utils` function now returns a template for utility functions, including argument parsing and data loading.

--- pyaoc/utils/languages/python/exercise.py
+++ pyaoc/utils/languages/python/exercise.py
@@ -1,3 +1,15 @@
 
 def create_python_exercise()-> str:
-    return ''
+    return """from .utils import parse_args, load_data
+
+def exercise() -> int:
+    args = parse_args()
+    data = load_data(args.data_path)
+
+    return 0
+
+if __name__ == '__main__':
+    result = exercise()
+    print(result)
+
+    """
--- pyaoc/utils/languages/python/utils.py
+++ pyaoc/utils/languages/python/utils.py
@@ -1,4 +1,21 @@
 def create_python_utils() -> str:
-    return ''
+    return """from argparse import ArgumentParser, Namespace
+
+class AocNamespace(Namespace):
+    data_path : str
+
+def parse_args():
+    parser = ArgumentParser('Advent of Code Exercise')
+    parser.add_argument('--data-path', type=str, action='store', required=True, help='Path to file input for exercise. Input or Example')
+    return parser.parse_args(namespace=AocNamespace())
+
+def load_data(data_path: str) -> str:
+    '''
+    Load Data from data_path to a string
+
+    '''
+    return open(data_path,'r').read()
+    
+    """
 
 
